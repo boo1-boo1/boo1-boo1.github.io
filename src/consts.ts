5324b00c0561fb97361ecf2c4071070a4f236862
Bhuvan Natesh <bhuvan.natesh@gmail.com>
2024-11-01 14:58:09 -0400

    Initial Commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Bhuvan\'s Blog';
+export const SITE_DESCRIPTION = 'Come in, d!ck#e@d!';
---
+++ src/content/blog/first-post.md
@@ -0,0 +1,8 @@
+---
+title: "First post"
+description: "Lorem ipsum dolor sit amet"
+pubDate: "November 1 2024"
+heroImage: "/blog-placeholder-3.jpg"
+---
+
+Well, what do you want me to say?
